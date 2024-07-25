f30641790a2ebddd133a8e4d9920db00600cba05
Eve Stroud <evestroud89@gmail.com>
2024-07-25 09:28:44 -0400

    Lab 3 randomized tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,61 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> noResizing = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        for (int i = 0; i < 3; i += 1) {
+            noResizing.addLast(i);
+            buggy.addLast(i);
+        }
+
+        for (int i = 0; i < 3; i += 1) {
+            int nRRemoved = noResizing.removeLast();
+            int bRemoved = buggy.removeLast();
+            assertEquals(nRRemoved, bRemoved);
+        }
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            switch (operationNumber) {
+                case 0:
+                    // addLast
+                    int randVal = StdRandom.uniform(0, 100);
+                    L.addLast(randVal);
+                    B.addLast(randVal);
+                    break;
+                case 1:
+                    // size
+                    int LSize = L.size();
+                    int BSize = B.size();
+                    assertEquals(LSize, BSize);
+                    break;
+                case 2:
+                    //getLast
+                    if (L.size() > 0) {
+                        int LLast = L.getLast();
+                        int BLast = B.getLast();
+                        assertEquals(LLast, BLast);
+                    }
+                    break;
+                case 3:
+                    // removeLast
+                    if (L.size() > 0) {
+                        int LRemovedLast = L.removeLast();
+                        int BRemovedLast = B.removeLast();
+                        assertEquals(LRemovedLast, BRemovedLast);
+                    }
+                    break;
+            }
+        }
+    }
 }
