79f3a17fe8d8051828e5c7a40b912c67f9b7b466
Eve Stroud <evestroud89@gmail.com>
2024-07-29 17:05:41 -0400

    Project 1 : API checker - MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -4,63 +4,24 @@ import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
     private final Comparator<T> defaultComparator;
-    private T defaultLargest = null;
 
     public MaxArrayDeque(Comparator<T> c) {
         defaultComparator = c;
     }
 
-    @Override
-    public void addFirst(T item) {
-        if (defaultLargest == null || defaultComparator.compare(item, defaultLargest) > 0) {
-            defaultLargest = item;
-        }
-
-        super.addFirst(item);
-    }
-
-    @Override
-    public void addLast(T item) {
-        if (defaultLargest == null || defaultComparator.compare(item, defaultLargest) > 0) {
-            defaultLargest = item;
-        }
-
-        super.addLast(item);
-    }
-
-    @Override
-    public T removeFirst() {
-        T item = super.removeFirst();
-
-        if (defaultComparator.compare(item, defaultLargest) == 0) {
-            defaultLargest = max(defaultComparator);
-        }
-        return item;
-    }
-
-    @Override
-    public T removeLast() {
-        T item = super.removeLast();
-
-        if (defaultComparator.compare(item, defaultLargest) == 0) {
-            defaultLargest = max(defaultComparator);
-        }
-        return item;
-    }
-
     public T max() {
-        return defaultLargest;
+        return max(defaultComparator);
     }
 
     public T max(Comparator<T> customComparator) {
-        T customLargest = null;
+        T largest = null;
 
         for (int i = 0; i < size(); i += 1) {
             T item = get(i);
-            if (customLargest == null || customComparator.compare(item, customLargest) > 0) {
-                customLargest = item;
+            if (largest == null || customComparator.compare(item, largest) > 0) {
+                largest = item;
             }
         }
-        return customLargest;
+        return largest;
     }
 }
